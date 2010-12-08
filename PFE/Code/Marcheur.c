95130cbeadc2bd07ad9f15ffe74818c008002f7e
fab0670312047 <fab0670312047@625544ac-cba7-7b1b-33c3-a1080720e865>
2010-12-08 14:35:12 +0000

    + Deplacement d'un fichier + Ajout du début du code du marcheur

---
+++ PFE/Code/Marcheur.c
@@ -0,0 +1,18 @@
+#include stdio.h
+#define N 100
+
+float F[N];
+/* ModeleSimple du marcheur 
+Ce code rend compte du premier modele ou unqiement la distance importe
+*/
+float ModeleSimple(int Noperation)
+{ /* e[0] contient le rayon de e*/
+int i;
+float F=0.5, WAF;
+
+for(i=1;i<=NOperation;i++)
+  {
+    Fproduit = Fproduit * F;
+  }
+WAF=1-Fproduit;
+return WAF;
