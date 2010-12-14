3d8c00a609dff2a39dc8caf42ad566f8ed5f4ba1
fab0670312047 <fab0670312047@625544ac-cba7-7b1b-33c3-a1080720e865>
2010-12-14 08:55:05 +0000

    + Ajout de spec structure + Début de code

--- PFE/Code/Marcheur.c
+++ PFE/Code/Marcheur.c
@@ -8,7 +8,7 @@ Ce code rend compte du premier modele ou unqiement la distance importe
 */
 float ModeleSimple(int Noperation)
 { /* e[0] contient le rayon de e*/
-int i;
+int i,j, nbrProduit;
 float F=0.5, WAF;
 
 for(i=1;i<=NOperation;i++)
