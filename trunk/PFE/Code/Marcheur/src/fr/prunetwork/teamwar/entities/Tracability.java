a1eea10aa15cc8a02a3f6177d0e210ecfcfa76c4
jpierre03 <jpierre03@625544ac-cba7-7b1b-33c3-a1080720e865>
2011-01-08 19:13:18 +0000

    + Changement de mon nom d'auteur par un pseudo (adresse mail en fait)     (suite à la demande de l'équipe Team-W@R)

--- trunk/PFE/Code/Marcheur/src/fr/prunetwork/teamwar/entities/Tracability.java
+++ trunk/PFE/Code/Marcheur/src/fr/prunetwork/teamwar/entities/Tracability.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright (C) 2010 Team-W@R (team-war@prunetwork.fr)
- * 
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation\t either version 3 of the License\t or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful\t
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not\t see <http://www.gnu.org/licenses/>.
  */
@@ -20,7 +20,7 @@ import fr.prunetwork.teamwar.utilities.MyDate;
 
 /**
  *
- * @author Jean-Pierre Prunaret (jpierre03+twar@prunetwork.fr)
+ * @author jpierre03+teamwar@prunetwork.fr
  * @author garciaf
  */
 public class Tracability {
--- trunk/PFE/Code/Marcheur/src/fr/prunetwork/teamwar/extractor/BatchExtractor.java
+++ trunk/PFE/Code/Marcheur/src/fr/prunetwork/teamwar/extractor/BatchExtractor.java
@@ -1,16 +1,16 @@
 /*
  *  Copyright (C) 2010 Team-W@R (team-war@prunetwork.fr)
- * 
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -29,7 +29,7 @@ import java.util.Map;
 
 /**
  *
- * @author Jean-Pierre Prunaret (jpierre03+twar@prunetwork.fr)
+ * @author jpierre03+teamwar@prunetwork.fr
  * @author garciaf
  */
 public class BatchExtractor {
@@ -43,7 +43,7 @@ public class BatchExtractor {
     private static void createBatchTracabilityRelation(
             Collection<Tracability> tracabilitys,
             Map<String, Tracability> batchTracabilityMap) {
-        //populate the index of batch 
+        //populate the index of batch
         for (Iterator<Tracability> it = tracabilitys.iterator(); it.hasNext();) {
 
             Tracability tracability = it.next();
